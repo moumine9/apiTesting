5074a36e05f15743e5cb753ddf23ec08349e5b0b
Abdoul-Moumine Mahamane Moussa <maha@premiertech.com>
2019-03-15 11:57:00 -0400

    Huge modification

--- apiTesting/helpers.py
+++ apiTesting/helpers.py
@@ -3,16 +3,81 @@
 
 import json
 import faker
-from htmlBuilder.tags import *
-from htmlBuilder.attributes import Class, InlineStyle
-from yattag import Doc
-
 from jinja2 import Template
 
 page = """
 
+<!doctype html>
+<html class="no-js" lang="">
 
+<head>
+  <meta charset="utf-8">
+  <title>API Tests Results</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://bootswatch.com/4/litera/bootstrap.min.css">
+
+</head>
+
+<body>
+  <!--[if IE]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+
+  <style>
+  body{margin-top:5%; margin-bottom:5%;}
+  table{margin:5%;}
+  </style>
+
+  <div class="container">
+
+    <h1 class='text-center'>{{ test_name }}</h1>
+    <p class='text-center text-muted'>{{ test_url }}</p>
+
+    <table class="table table-hover table-striped table-bordered col-10">
+
+        <thead>
+
+            <tr class="table-dark">
+                <th scope="col">Name</th>
+                <th scope="col">Url</th>
+                <th scope="col" class="text-center">Method</th>
+                <th scope="col" class="text-center">Results</th>
+            </tr>
+
+        </thead>
+
+        <tbody>
+
+        {% for test in tests %}
+
+            <tr>
+                <th> {{ test.name }} </th>
+                <th> {{ test.url }} </th>
+                <td class="text-center"> {{ test.method }} </td>
+                <td class="text-center">
+
+                    {% if test.result.status_code == 'FAILED' or test.result.body == 'FAILED' %}
+                        <i class="fas fa-circle text-danger fa-md"></i>
+                    {% else %}
+                        <i class="fas fa-circle text-success fa-md"></i>
+                    {%- endif %}
+                </td>
+            </tr>
+
+        {% endfor %}
+
+        </tbody>
+
+    </table> 
+
+    </div>
+
+</body>
+
+</html>
 
 """
 
@@ -22,24 +87,22 @@ def clean_response(response, test):
     
     req["url"] = test["url"]
     req["name"] = test["name"]
+    req["method"] = test['method']
     req["result"] = {"status_code":"N/A", "body":"N/A"}
-    req["result"]["stat"] = ("FAILED", "OK")[response.status_code == int(test['response']['status_code']) ]
     
     if( 'status_code' in test['response'] ):
         req['result']['status_code'] = ("FAILED", "OK")[ int(response.status_code) == int(test['response']['status_code']) ]
     
     if( 'body' in test['response'] ):
-        req['result']['body'] = ("FAILED", "OK")[ json.dumps(response.json()) == json.dumps(test['response']['body']) ]
+        req['result']['body'] = ("FAILED", "OK")[ response.json() == test['response']['body'] ]
     
     return req
 
 
 #Function to build the html page
-def generate_body(results):
-    doc, tag, text = Doc().tagtext()
+def generate_body(tests_info,results):
+    template = Template(page)
 
-    with tag('html'):
-        with tag('head'):
-            with tag('link')
+    return template.render(test_name = tests_info["name"], test_url = tests_info["base_url"], tests = results)
 
-    print(doc.getvalue())  
+
--- apiTesting/main.py
+++ apiTesting/main.py
@@ -8,10 +8,10 @@ from helpers import *
 
 stream = open('tests.yaml', 'r')
 tests_data = load(stream)
+tests_info = { "name" : tests_data["name"], "base_url": tests_data["base_url"] }
 tests_results = []
 
 for test in tests_data["test_cases"]:
-    print("Test : %s | %s " % (test["name"], test["method"]) )
     if ( (test['method'] == 'POST' or test['method'] == 'PUT' or test['method'] == 'UPDATE' or test['method'] == 'PATCH') and ('data' not in test) ):
         raise ValueError("No post data provided")
     
@@ -24,12 +24,31 @@ for test in tests_data["test_cases"]:
         tests_results.insert(-1,clean_response(req, test))
 
     if(test['method'] == 'HEAD'):
-        req = requests.head(tests_data['base_url'] + test['url'], data = test['data'])
+        req = requests.head(tests_data['base_url'] + test['url'], data = json.dumps(test['data']) )
         tests_results.insert(-1,clean_response(req, test))
 
     if(test['method'] == 'GET'):
         req = requests.get(tests_data['base_url'] + test['url'])
         tests_results.insert(-1, clean_response(req, test) )
 
-print(tests_results)
-generate_body(tests_results)
+for test in tests_results:
+    res = "N/A"
+    status_code = test['result']['status_code']
+    body = test['result']['body']
+
+    if( (status_code == body == "OK") or (status_code == body == "N/A")  ):
+        res = "OK"
+    elif( status_code == "OK" and body == "N/A" ):
+        res = "OK"
+    elif( status_code == "N/A" and body == "OK" ):
+        res = "OK"
+    else:
+        res = "FAILED"
+
+    print("Test : %s | %s | %s" % (test["name"], test["method"], res) )
+
+
+page = generate_body(tests_info,tests_results)
+
+output = open('results.html','w')
+output.write(page)
